94bdd2728e4d772963083ad9e4ff7f2226d082d7
Josh Gillham <usajoshgillham@gmail.com>
2013-03-16 23:26:17 -0600

    Rewrote new questions.

--- src/CS1/ArrayQuestions.java
+++ src/CS1/ArrayQuestions.java
@@ -88,69 +88,73 @@ public class ArrayQuestions extends framework.Quiz {
     }
     
     /**
-     * Question #B.3)
-     * The method return a new array from the combination of other two arrays.
-     *  For example, if the first array is { 5, 15, 9 } and the second array is
-     *  { 7, 9, 11 }, then the result will be { 5, 15, 9, 7, 9, 11 }. 
+     * Question #C.1)
+     * The method returns a new array which is the combination of two other 
+     *  arrays. For example, if the first array is { 5, 15, 9 } and the second 
+     *  array is { 7, 9, 11 }, then the result will be { 5, 15, 9, 7, 9, 11 }. 
      * 
      * @param array is the array to look through.
      * 
-     * @return the new array with the odd elements.
+     * @return the new combination of arrays.
      */
-    public static int[] changeArray103( int[] array1, int[] array2 ) {
+    public static int[] combineArrays( int[] array1, int[] array2 ) {
         //TODO delete the line below and write your solution.
         throw new UnsupportedOperationException();
     }
     
     /**
-     * Question #B.4)
-     * The method modifies array by adding a number n to each element in the 
-     *  array.
+     * Question #D.1)
+     * The method modifies an array by adding a number n to each element in the 
+     *  array. For example, if n = 3 and the array is { 2, 1, 5, 7 } then the 
+     *  result will be { 5, 4, 8, 10 }.
      * 
      * @param array is the array to look through.
      * 
-     * @return the new array with the odd elements.
+     * @return the new modified array.
      */
-    public static int[] changeArray104( int[] array1, int[] array2 ) {
+    public static int[] incrementArray( int[] array, int n ) {
         //TODO delete the line below and write your solution.
         throw new UnsupportedOperationException();
     }
     
     /**
-     * Question #B.5)
-     * The method return the total of an array.
+     * Question #E.1)
+     * The method return the sum of an array. For example, an array of { 14, 2
+     *  8, 6, 3 } should return 33.
      * 
      * @param array is the array to look through.
      * 
-     * @return the new array with the odd elements.
+     * @return the sum of the array.
      */
-    public static int[] changeArray105( int[] array1 ) {
+    public static int sumArray( int[] array ) {
         //TODO delete the line below and write your solution.
         throw new UnsupportedOperationException();
     }
     
     /**
-     * Question #B.6)
-     * The method return the maximum value of an array.
+     * Question #F.1)
+     * The method return the maximum value of an array. For example, if there is
+     *  an array with { 4, 5, 2, 3 }, then the maximum will be 5.
      * 
      * @param array is the array to look through.
      * 
-     * @return the new array with the odd elements.
+     * @return the maximum value of an array.
      */
-    public static int[] changeArray106( int[] array1 ) {
+    public static int[] getArrayMax( int[] array ) {
         //TODO delete the line below and write your solution.
         throw new UnsupportedOperationException();
     }
     
     /**
-     * Question #B.7)
-     * The method returns the minimum value of an array.
+     * Question #F.2)
+     * The method returns the minimum value of an array. For example, if there
+     *  is an array with { 11, 17, 19, 3, 4 } the result will be 3.
      * 
      * @param array is the array to look through.
      * 
-     * @return the new array with the odd elements.
+     * @return the minimum value of an array.
      */
-    public static int[] changeArray107( int[] array1 ) {
+    public static int[] getArrayMin( int[] array ) {
         //TODO delete the line below and write your solution.
         throw new UnsupportedOperationException();
     }
