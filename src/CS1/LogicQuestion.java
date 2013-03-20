ebafcc458eb3fc2b54076a1377da128802d7af49
StarCute <cdean11@msudenver.com>
2013-03-19 22:26:22 -0600

    Update LogicQuestion.java

--- src/CS1/LogicQuestion.java
+++ src/CS1/LogicQuestion.java
@@ -1,5 +1,27 @@
-//LoopQuestion.java
-// Modulus question
-// If, else, switch
-// Java API research and implement
-// Barcode check
+// This is some C code that I wrote a long time ago. I'd
+// have to translate it to java code but I think the idea 
+// is pretty cool because it does require a little bit of 
+// thinking and does use some of the stuff that I was talking
+// about with modulus, using the scanner class, and just printing
+// output to the console.
+
+/* Computes a Universal Product Code check digit */
+
+#include <stdio.h>
+
+int main(void)
+{
+  int d, i1, i2, i3, i4, i5, j1, j2, j3, j4, j5,
+		first_sum, second_sum, total;
+
+	printf("Enter the 11 digit upc bar code: ");
+	scanf("%1d%1d%1d%1d%1d%1d%1d%1d%1d%1d%1d", &d, &i1, &i2, &i3, &i4, &i5, &j1, &j2, &j3, &j4, &j5);
+
+	first_sum = d + i2 + i4 + j1 + j3 + j5;
+	second_sum = i1 + i3 +i5 + j2 + j4;
+	total = 3 * first_sum + second_sum;
+
+	printf("Check digit: %d\n", 9 - ((total - 1) % 10));
+
+	return 0;
+}
